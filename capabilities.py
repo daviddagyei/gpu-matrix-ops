231a6ca846567db2696a83d84460f13cd1cd337a
daviddagyei <daviddagyei@users.noreply.github.com>
2026-04-28 21:42:11 -0500

    Prune repo to GPU_fundamental

--- capabilities.py
+++
@@ -1,9 +0,0 @@
-CAPABILITY_PROFILES = {
-    "alpaca_crypto_long_only": {
-        "name": "alpaca_crypto_long_only",
-        "supports_long": True,
-        "supports_short": False,
-        "supports_fractional": True,
-        "supports_pyramiding": True,
-    }
-}
